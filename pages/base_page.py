55b4de23f8bbc8593376c8cddfed44de0e1365c2
timofey-kosa <46551995+timofey-kosa@users.noreply.github.com>
2021-02-02 17:47:40 +0200

    base_page(is_element_present)

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,11 +1,21 @@
 from selenium import webdriver
+import time
+from selenium.common.exceptions import NoSuchElementException
 
 
 class BasePage:
-    def __init__(self, browser: webdriver, url):
+    def __init__(self, browser: webdriver, url, timeout=10):
         self.browser = browser
         self.url = url
+        self.browser.implicitly_wait(timeout)
 
     def open(self):
         self.browser.get(self.url)
+
+    def is_element_present(self, how, what):
+        try:
+            self.browser.find_element(how, what)
+        except NoSuchElementException:
+            return False
+        return True
 
